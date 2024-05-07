5a538cf6c0e082e7a1acb4048be449bfbaf30971
Marco <leandroantoniotelles@gmail.com>
2024-05-07 16:24:15 -0300

    100% ATUALIZADO, É RUIM DE ATURAR, BOMBA PATCH VIROU MODA TODO MUNDO QUER JOGAR, COM A NOSSA EQUIPE, NINGUEM BATE DE FRENTE, BOMBA PATCH É NERVOSO NÃO DA CHANCE AO CONCORRENTE, SE LIGA AI IRMAO , ESCUTE OQUE EU VOU DIZER QUEM FECHA COM O BOMBA PATCH SEMPRE VAI FORTALECER, PORQUE TUDO QUE É BOM, A GENTE REPETE, É POR ISSO QUE EU FECHO COM A EQUIPE BOMBA PATCH tanataaanyabahbhjbkajhbkajhmba jhabmjhbn jhjhb ahbajh

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,9 @@
     <title>BlindZone</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Freeman&display=swap" rel="stylesheet">
 
     <style>
         .fixed-bottom-row {
@@ -39,41 +42,70 @@
     <div class="container-fluid" style="width: 100%;">
         <div class="row bg-dark" style="padding-top: 15px; padding-bottom: 15px">
             <div class="col-4 custom-col">
-            <a href="#" class="btn custom-link imgfg" style="border-style: none;">
+                <a href="#" class="btn custom-link imgfg" style="border-style: none;">
                     <i class="bi bi-person-circle"></i><br>
                     <p>Perfil</p>
                 </a>
             </div>
-            <div class="col-4 custom-col" >
+            <div class="col-4 custom-col">
                 <img src="img/out.png" alt="" width="100%">
             </div>
             <div class="col-4 custom-col">
-            <a href="#" class="btn custom-link imgfg" style="border-style: none;">
+                <a href="#" class="btn custom-link imgfg" style="border-style: none;">
                     <i class="bi bi-gear"></i><br>
                     <p>Configurações</p>
                 </a>
             </div>
         </div>
     </div>
-
+    <div class="container">
+        <div class="row " style="background-color: gray;">
+            <center>
+                <h1 style="font-family:monospace;color:whitesmoke;margin-top:10px;margin-bottom:10px">CADASTRAR COMANDO</h1>
+            </center>
+        </div>
+        <br>
+        <div class="row">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label"><b>NOME DO COMANDO:</b></label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Informe o nome do comando">
+            </div>
+        </div><br>
+        <label for="exampleInputEmail1" class="form-label"><b>COMANDO:</b></label>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Selecione o comando</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
+        <br>
+        <label for="exampleInputEmail1" class="form-label"><b>Áudio: </b></label>
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1"><i class="bi bi-mic"></i></span>
+            <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1" placeholder="Fale o comando desejado">
+        </div>
+        <br>
+        <center><a href="#" class="btn btn-outline-success ">CADASTRAR</a></center>
+        <center><a href="#" class="btn btn-outline-danger " style="margin-top: 160px;">SAIR</a></center>
+    </div>
     <div class="fixed-bottom-row" style="padding-top: 15px; padding-bottom: 15px;background-color:dimgray">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-4 custom-col">
-                <a href="#" class="btn custom-link" style="border-style: none;">
-                    <i class="bi bi-fingerprint"></i><br>
+                    <a href="#" class="btn custom-link" style="border-style: none;">
+                        <i class="bi bi-fingerprint"></i><br>
                         <p>Digitais</p>
                     </a>
                 </div>
                 <div class="col-4 custom-col">
-                <a href="#" class="btn custom-link" style="border-style: none;">
-                    <i class="bi bi-mic"></i><br>
+                    <a href="#" class="btn custom-link" style="border-style: none;">
+                        <i class="bi bi-mic"></i><br>
                         <p>Comandos</p>
                     </a>
                 </div>
                 <div class="col-4 custom-col">
                     <a href="#" class="btn custom-link" style="border-style: none;">
-                       <i class="bi bi-bell imgf"></i><br>
+                        <i class="bi bi-bell imgf"></i><br>
                         <p class="imgf">Notificações</p>
                     </a>
                 </div>
